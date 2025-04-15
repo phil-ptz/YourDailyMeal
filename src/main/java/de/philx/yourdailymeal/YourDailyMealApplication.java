1b7dbf146b4b44d8ccc92fce82e52aa8169c0f00
Phil Potratz <potratzphill@gmail.com>
2025-04-15 16:43:31 +0200

    Initial commit

---
+++ src/main/java/de/philx/yourdailymeal/YourDailyMealApplication.java
@@ -0,0 +1,13 @@
+package de.philx.yourdailymeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDailyMealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourDailyMealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/philx/yourdailymeal/YourDailyMealApplicationTests.java
@@ -0,0 +1,13 @@
+package de.philx.yourdailymeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDailyMealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
